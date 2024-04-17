72c0819e2d0e2973ad88060d27f8424b170e54f8
PomPomSqL <167272073+PomPomSqL@users.noreply.github.com>
2024-04-17 05:04:01 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export default {
     port: 3000, // local test
-    WebURL: 'https://ashleyselfb.onrender.com/', // http://localhost:port/
+    WebURL: 'https://pompomv1.onrender.com', // http://localhost:port/
 }
